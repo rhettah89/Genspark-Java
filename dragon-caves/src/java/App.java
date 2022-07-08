c4880ce50fa4aaf84d7c4824c89117fb27f59c49
Rhetta H <rhetta.hovdal@gmail.com>
2022-07-08 16:50:16 -0700

    update

--- dragon-caves/src/java/App.java
+++ dragon-caves/src/java/App.java
@@ -1,37 +1,27 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class App {
-
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         String choice;
         String result;
         Scanner input = new Scanner(System.in);
 
         System.out.println(
-                "In front of you is a fork in the road leading to two caves. A storm is approaching so you hastily choose a path.\n Type left or right");
-
-        do {
-            choice = input.nextLine();
+                "In front of you is a fork in the road leading to two caves. A storm is approaching so you hastily choose a path.\n Type left or right\nOr X to exit");
+        choice = input.nextLine();
 
 
-            switch (choice) {
-                case "left":
-                    result = "You come upon a friendly looking dragon. It offers you roast pig and shows you a pile of maps";
+        if (choice.equals("left")) {
+            result = "You come upon a friendly looking dragon. It offers to fly you to our destination when the storm passes";
+        } else if (choice.equals("right")) {
+            result = "You burst into flames!";
+        } else {
+            result = "Your caught in the torrential rain. Your wet, cold and miserable";
+        }
+        System.out.println(result);
 
-                    break;
+    }}
 
-                case "right":
-                    result = "You burst into flames!";
-                    break;
 
-                default:
-                    result = "You turn around in circles dazed and confused. Choose a path before the storm comes!\n Type left or right";
-            }
 
-            input.close();
-
-            System.out.println(result);
-
-        } while (!choice.equals("left") || choice.equals("right"));
-    }
-}
