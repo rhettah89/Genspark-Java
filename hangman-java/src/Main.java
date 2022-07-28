b58b073fe80142c9a4a18802729be34c39e7039e
Rhetta H <rhetta.hovdal@gmail.com>
2022-07-28 08:10:48 -0700

    update missed letters

--- hangman-java/src/Main.java
+++ hangman-java/src/Main.java
@@ -9,31 +9,38 @@ public class Main {
     public static String wordView(String guess, String randomWord ){
         List<String> letters = new ArrayList<>();
         String word = "";
-
             for(int i = 0; i < randomWord.length(); i++){
                 letters.add("_");
                 }
-
-
         if(randomWord.contains(guess)){
             letters.set(randomWord.indexOf(guess), guess);
         }
         for(String letter : letters){
             word = (word + letter);
         }
-
-
         System.out.println(word);
         return word;
+    }
+
+    public static String missedLetters(String randomWord, String guess){
+        List<String> letters = new ArrayList<>();
+        String missed = "";
+        if(!randomWord.contains(guess)){
+            letters.add(guess);
+        }
+        for(String letter : letters){
+            missed = (missed + letter);
+        }
+        return letters.toString().replace("[", "").replace("]", "");
     }
 
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
         String randomWord = "";
-        String guess = " ";
-        boolean completed = false;
+        String guess = "d";
+        String space = "";
+        String board = "";
         String word = wordView(guess, randomWord);
-
         System.out.println(word);
 
         URI uri = URI.create("https://random-word-api.herokuapp.com/word");
@@ -44,21 +51,15 @@ public class Main {
             System.out.println( result.replace("\"", "").replace("[", "").replace("]", ""));
         }
 
-        String space = "";
-        String board = "";
+
         for(int i = 0; i < randomWord.length(); i++ ){
             space = space + " ";
             board = board + "=";
         }
         // Game Start
-//         guess = input.nextLine();
         System.out.println("\n\nWELCOME TO HANGMAN\n" + "+" + wordView(guess,randomWord) + "+");
-        System.out.println(space+ "|\n"  + space+ "|\n"  +  space+ "|\n" + " " + board+"\n\nMissed letters:\n\nGuess a letter");
-        while(!completed){
-            guess = input.nextLine();
-            System.out.println("\n\nWELCOME TO HANGMAN\n" + "+" + wordView(guess,randomWord) + "+");
-            System.out.println(space+ "|\n"  + space+ "|\n"  +  space+ "|\n" + " " + board+"\n\nMissed letters:\n\nGuess a letter");
-        }
+        System.out.println(space+ "|\n"  + space+ "|\n"  +  space+ "|\n" + " " + board+"\n\nMissed letters: " + missedLetters(randomWord, guess) + "\n\nGuess a letter");
+        guess = input.nextLine();
     }
 
     private static String convertInputStreamToString(InputStream is) throws IOException {
